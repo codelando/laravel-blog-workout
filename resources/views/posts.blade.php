1604ce16121528f4ab573a6cc4452fea8cbc8f83
codelando <codelando@gmail.com>
2023-09-14 19:54:13 -0300

    Added blade syntax and components.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-    <?php foreach ($posts as $post) : ?>
+<x-layout>
+    @foreach ($posts as $post)
         <article>
             <h1>
-                <a href="posts/<?= $post->slug; ?>">
-                    <?= $post->title; ?>
+                <a href="posts/{{ $post->slug; }}">
+                    {{ $post->title; }}
                 </a>
             </h1>
-            <p><?= $post->excerpt; ?></p>
+            <div>{{ $post->excerpt; }}</div>
         </article>
-    <?php endforeach; ?>
-</body>
-</html>
+    @endforeach
+</x-layout>
